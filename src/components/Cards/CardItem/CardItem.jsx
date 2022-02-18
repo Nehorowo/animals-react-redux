5d9cbfceff18e2acb5c9e91202028b3996e591d5
Nehorowo <trianglek4nz@gmail.com>
2022-02-18 14:30:05 +0300

    add some css for delete button

--- src/components/Cards/CardItem/CardItem.jsx
+++ src/components/Cards/CardItem/CardItem.jsx
@@ -28,7 +28,20 @@ const CardItem = ({ name, image_link, onClickLike, onClickDelete, id, likes, die
           </g>
         </svg>
       </div>
-      <button onClick={handleDelete}>Удалить</button>
+      <div className={styles.trashcan_back}>
+        <svg
+          onClick={handleDelete}
+          className={styles.trashcan}
+          width="22px"
+          height="22px"
+          viewBox="0 0 32 32">
+          <rect x="12" y="12" width="2" height="12" />
+          <rect x="18" y="12" width="2" height="12" />
+          <path d="M4,6V8H6V28a2,2,0,0,0,2,2H24a2,2,0,0,0,2-2V8h2V6ZM8,28V8H24V28Z" />
+          <rect x="12" y="2" width="8" height="2" />
+          <rect fill="none" width="32" height="32" />
+        </svg>
+      </div>
     </div>
   );
 };
